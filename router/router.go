a19593d029b3ede64606964b6b062e59e243a819
writethesky <writethesky@163.com>
2021-11-13 16:39:03 +0800

    fix: cors

--- router/router.go
+++ router/router.go
@@ -67,8 +67,10 @@ func auth(c *gin.Context) {
 }
 
 func cors(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Host)
+
+	c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
